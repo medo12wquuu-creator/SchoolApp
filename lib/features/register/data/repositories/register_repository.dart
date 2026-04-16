524043c441924fbf46d88f8e1ab66327df69e464
Mohammad Hameed <medo12wquuu@gmail.com>
2026-04-16 20:58:38 +0300

    handling register status

--- lib/features/register/data/repositories/register_repository.dart
+++ lib/features/register/data/repositories/register_repository.dart
@@ -1,6 +1,5 @@
 import 'package:school_app/features/register/data/datasources/register_remote_data_source.dart';
-
-import '../models/register_response.dart';
+import 'package:school_app/features/register/data/models/register_response.dart';
 
 class RegisterRepository {
   final RegisterRemoteDataSource remote;
@@ -14,8 +13,7 @@ class RegisterRepository {
     required String phone,
     required String password,
   }) async {
-    // إرسال البيانات إلى الـ RemoteDataSource
-    final data = await remote.register({
+    final result = await remote.register({
       "first_name": firstName,
       "last_name": lastName,
       "email": email,
@@ -23,7 +21,21 @@ class RegisterRepository {
       "password": password,
     });
 
-    // تحويل JSON إلى Model
-    return RegisterResponse.fromJson(data);
+    final status = result["statusCode"];
+    final data = result["data"];
+
+    if (status == 200) {
+      return RegisterResponse(
+        success: true,
+        message: "Accepted",
+        statusCode: 200,
+      );
+    } else {
+      return RegisterResponse(
+        success: false,
+        message: "Pending approval",
+        statusCode: status,
+      );
+    }
   }
 }
