44ddf48aba851ad1d86f157f4af7a8ca2eee64ba
Mohammad Hameed <medo12wquuu@gmail.com>
2026-04-16 20:58:13 +0300

    define statuscode in my  rezponce

--- lib/features/register/data/models/register_response.dart
+++ lib/features/register/data/models/register_response.dart
@@ -1,13 +1,19 @@
 class RegisterResponse {
   final bool success;
   final String message;
+  final int statusCode;
 
-  RegisterResponse({required this.success, required this.message});
+  RegisterResponse({
+    required this.success,
+    required this.message,
+    required this.statusCode,
+  });
 
   factory RegisterResponse.fromJson(Map<String, dynamic> json) {
     return RegisterResponse(
       success: json['success'] ?? false,
       message: json['message'] ?? '',
+      statusCode: json['statusCode'] ?? 0,
     );
   }
 }
